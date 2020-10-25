758b27aadb161927fa9d231c2b9fb54eee5d2735
Landon flop <68473617+ch1ck3n-byte@users.noreply.github.com>
2020-10-24 20:42:44 -0600

    Update dogescript.js

--- dogescript.js
+++ dogescript.js
@@ -66,14 +66,24 @@ var doge = {
 		xhttp.open("GET", "https://distracted.ch1cken.repl.co/", true);
 		xhttp.send();
 	},
-	download: function (src) {
-		var a;
-		a = document.createElement("a");
-		a.href = src;
-		a.download = "download"
-		a.click()
-		a.remove
-
+	download: function (data, filename, type) {
+		var file = new Blob([data], {
+			type: type
+		});
+		if (window.navigator.msSaveOrOpenBlob) // IE10+
+			window.navigator.msSaveOrOpenBlob(file, filename);
+		else { // Others
+			var a = document.createElement("a"),
+				url = URL.createObjectURL(file);
+			a.href = url;
+			a.download = filename;
+			document.body.appendChild(a);
+			a.click();
+			setTimeout(function () {
+				document.body.removeChild(a);
+				window.URL.revokeObjectURL(url);
+			}, 0);
+		}
 	},
 	getAbsoluteUrl: function () {
 		var a;
